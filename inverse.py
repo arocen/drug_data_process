9d5dc0c5ee498790c34b3e3841f2f2b99fc6dccf
arocen <aufger1913@hotmail.com>
2023-11-05 20:30:21 +0800

    complete inverse.py

--- inverse.py
+++ inverse.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 load_dotenv() # load .env file
 
 ATC2ICD_path = os.environ.get("ATC2ICD_path")
+ICD2ATC_path = os.environ.get("ICD2ATC_path")
 
 def read(original_path=ATC2ICD_path)->pd.DataFrame:
     '''
@@ -58,12 +59,22 @@ def transfer(original_df)->dict:
 
     # 遍历, 计算逆向概率
     # 假设ATC-4的先验分布服从均匀分布, 平均计算不同年龄段和不同性别条件下的概率
-    # 对于ICD-9的每一个取值, 获取对应的ATC4取值和概率
-    group_df = original_df.groupby(["ICD", "ATC"], dropna=True).mean()
+
+    # 对于ICD-9的每一个取值, 获取对应的ATC4取值和概率, 对概率求和
+    group_df = original_df.groupby(["ICD", "ATC"]).sum()
+
+    # 对概率取均值，分母为所有表的数量
+    # group_df["Prob"] = group_df["Prob"] / 8 # 归一化以后跟不除的结果是一样的
+
+    # 归一化
+    group_df["Prob"] = group_df["Prob"] / original_df.groupby(["ICD"]).sum(numeric_only=True)["Prob"]
+    
+    # 去除概率为0.0的行
+    group_df = group_df[group_df["Prob"] != 0.00]
     print(group_df)
 
-    # 加总同一个ATC4取值对应概率，归一化
-    # 存储为字典
+    # 存储为excel
+    group_df.to_excel(ICD2ATC_path)
     return
 
 # test
