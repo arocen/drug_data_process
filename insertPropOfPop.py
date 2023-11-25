735152e7e5a1712337648e9ff2f4503f843dbd65
arocen <aufger1913@hotmail.com>
2023-11-25 21:22:42 +0800

    add to-do comments to insertProOfPop.py

---
+++ insertPropOfPop.py
@@ -0,0 +1,8 @@
+# Insert proportion of population column to IHME csv files according to sex_name and age_name columns
+# Given sex_id, age_id, and year, look up proportion of population in China.
+
+# To-do:
+# 1. Load China's population data by year.
+# 2. Get sex_id, age_id, and year from columns of IHME csv files.
+# 3. Look up and calculate proportion of population from population data.
+# 4. Insert proportion of population to a new column in IHME csv files.
