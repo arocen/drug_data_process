3b2fd74113bdddd94921fd16b4321535266ee00f
arocen <aufger1913@hotmail.com>
2024-01-16 17:50:04 +0800

    use a naive way to represent incidence

--- calcIncidence.py
+++ calcIncidence.py
@@ -6,14 +6,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def calcIncidencePerICD10(df:pd.dataframe):
+    # TODO: update the algorithm if data from other source is available
     '''
     Calculate incidence per icd code.
     
     - df: dataframe about patients discharged from hosiptal per ICD-10 code. Processed from 中国卫生年鉴.
+
+    Return 2 df with 2 columns. One is range of ICD10, another is percentage of patients discharged from hosiptal.
     '''
 
-
-    return result_df
+    return df["ICD10", "疾病构成(%)"]
 
 def calcIncidencePerICD10FromExcel(file:str, sheet:str='sheet1'):
     '''Calculate incidence from Excel instead of dataframe.'''
